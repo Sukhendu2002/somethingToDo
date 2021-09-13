84009de961a4f73b71b1f2faef3933febe05e19f
sukhendu2002 <sukhenduguria2017@gmail.com>
2021-09-13 10:56:51 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [text, setText] = useState("Genarate Ideas");
+  const [content, setcontent] = useState("");
+  const URL = "http://www.boredapi.com/api/activity/";
+  const handleClick = () => {
+    setText("Generating More Ideas");
+    axios.get(URL).then((response) => {
+      setcontent(response.data.activity);
+    });
+  };
+  return (
+    <div className="App">
+      <h1 className="App-header">getting bored At Home</h1>
+      <button className="App-button" onClick={handleClick}>
+        {text}
+      </button>
+      <p className="App-content">{content}</p>
+    </div>
+  );
+}
+
+export default App;
